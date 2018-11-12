98455ab14c2f47325bd1b215eec5d3466e8ce9f6
Daniel Berninghoff <daniel.berninghoff@gmail.com>
2018-11-12 10:10:15 +0100

    Change arrow color from magenta to green if command returns 0 and add CONDA environment to prompt

--- pure.zsh
+++ pure.zsh
@@ -182,8 +182,9 @@ prompt_pure_precmd() {
 	psvar[12]=
 	# When VIRTUAL_ENV_DISABLE_PROMPT is empty, it was unset by the user and
 	# Pure should take back control.
-	if [[ -n $VIRTUAL_ENV ]] && [[ -z $VIRTUAL_ENV_DISABLE_PROMPT || $VIRTUAL_ENV_DISABLE_PROMPT = 12 ]]; then
-		psvar[12]="${VIRTUAL_ENV:t}"
+        if [[ -n $VIRTUAL_ENV  || -n $CONDA_DEFAULT_ENV ]] && [[ -z $VIRTUAL_ENV_DISABLE_PROMPT || $VIRTUAL_ENV_DISABLE_PROMPT = 12 ]]; then
+		psvar12_=${VIRTUAL_ENV:-$CONDA_DEFAULT_ENV}
+		psvar[12]="${psvar12_:t}"
 		export VIRTUAL_ENV_DISABLE_PROMPT=12
 	fi
 
@@ -585,7 +586,7 @@ prompt_pure_setup() {
 	PROMPT='%(12V.%F{242}%12v%f .)'
 
 	# prompt turns red if the previous command didn't exit with 0
-	PROMPT+='%(?.%F{magenta}.%F{red})${prompt_pure_state[prompt]}%f '
+	PROMPT+='%(?.%F{green}.%F{red})${prompt_pure_state[prompt]}%f '
 
 	# Store prompt expansion symbols for in-place expansion via (%). For
 	# some reason it does not work without storing them in a variable first.
